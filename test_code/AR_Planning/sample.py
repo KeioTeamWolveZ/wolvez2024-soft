227e0240781a4ecbfd7e047328aafc640f242304
suzukiYU000 <syuma2001@keio.jp>
2024-04-17 18:55:38 +0900

    camera

---
+++ test_code/AR_Planning/sample.py
@@ -0,0 +1,20 @@
+import cv2
+
+# カメラを開く
+cap = cv2.VideoCapture(0)
+
+while True:
+    # 画像をキャプチャする
+    ret, frame = cap.read()
+
+    # 画像を表示する
+    cv2.imshow("Image", frame)
+
+    # `q`キーを押すとループを終了する
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# カメラを閉じる
+cap.release()
+# すべてのウィンドウを閉じる
+cv2.destroyAllWindows()
