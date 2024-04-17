46875eec5bb8c1423aad16eb3c51ccd0c092e4aa
suzukiYU000 <syuma2001@keio.jp>
2024-04-17 19:41:15 +0900

    camera

--- test_code/AR_Planning/sample.py
+++ test_code/AR_Planning/sample.py
@@ -1,12 +1,15 @@
 import cv2
+from picamera2 import Picamera2
 
+picam2 = Picamera2()
 # カメラを開く
 cap = cv2.VideoCapture(1)
 
 while True:
     # 画像をキャプチャする
-    ret, frame = cap.read()
-
+    # ret, frame = cap.read()
+    img = picam2.capture_array()
+    img = img[:,:,3]
     # 画像を表示する
     cv2.imshow("Image", frame)
 
