9c84458550dd19979e5f11a7ea3458ddf59cc1cd
suzukiYU000 <syuma2001@keio.jp>
2024-04-29 14:41:46 +0900

    test:outlier_removal

--- test_code/AR_Planning/detect_marker.py
+++ test_code/AR_Planning/detect_marker.py
@@ -7,6 +7,7 @@ from libcamera import controls
 from datetime import datetime
 from correction import Correct
 from polar import polar_change
+from AR_outlier import outlier
 
 # カメラのキャプチャ
 # cap = cv2.VideoCapture(1)
@@ -24,12 +25,17 @@ distortion_coeff = np.load("../../dist.npy")
 size = (1800, 1000)
 config = picam2.create_preview_configuration(
             main={"format": 'XRGB8888', "size": size})
+
 picam2.align_configuration(config)
 picam2.configure(config)
 picam2.start()
-picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+# picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+picam2.set_controls({"AfMode":0,"LensPosition":4.5})
 
 VEC_GOAL = [0.0,0.1968730025228114,0.3]
+ultra_count = 0
+prev = np.array([0,0,0])
+TorF = True
 
 
 while True:
@@ -39,7 +45,7 @@ while True:
     height = frame.shape[0]
     width = frame.shape[1]
 
-    print(height,width)
+    # print(height,width)
     # if not ret:
     #     break
 
@@ -55,7 +61,7 @@ while True:
         for i in range(len(ids)):
             if ids[i] in [0,1,2,3,4,5]:
                 image_points_2d = np.array(corners[i],dtype='double')
-                print(corners[i])
+                # print(corners[i])
 
                 rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners[i], marker_length, camera_matrix, distortion_coeff)
                 tvec = np.squeeze(tvec)
@@ -70,12 +76,20 @@ while True:
                 # オイラー角への変換
                 euler_angle = cv2.decomposeProjectionMatrix(proj_matrix)[6] # [deg]
 
+                # if ultra_count == 0:
+                #     prev = tvec
+                #     ultra_count = 1
+                # else:
+                #     TorF, prev = outlier(tvec, prev) # true:correct,false:outlier
+                #     if TorF:
                 print("x : " + str(tvec[0]))
                 print("y : " + str(tvec[1]))
                 print("z : " + str(tvec[2]))
                 print("roll : " + str(euler_angle[0]))
                 print("pitch: " + str(euler_angle[1]))
                 print("yaw  : " + str(euler_angle[2]))
+
+
                 # 発見したマーカーから1辺が30センチメートルの正方形を描画
                 # aruco.drawAxis(frame, camera_matrix, distortion_coeff, rvec, tvec, 0.1)
                 # aruco.drawDetectedMarkers(frame, corners, ids, (0, 0, 255))
@@ -86,12 +100,12 @@ while True:
                 point_3d = np.array([[tvec[0], tvec[1], tvec[2]]], dtype=np.float64)
                 imgpts, jac = cv2.projectPoints(point_3d,rvec, tvec, camera_matrix, distortion_coeff)
                 cv2.line(frame, (width//2,0), (width//2,height),(255,255,0))
-                print(imgpts)
-                print("ID:",ids[i])
+                # print(imgpts)
+                # print("ID:",ids[i])
                 distance, angle = Correct(tvec,VEC_GOAL)
-                print("kabuto_function:",distance,angle)
+                # print("kabuto_function:",distance,angle)
                 polar_exchange = polar_change(tvec)
-                print("yunosu_function:",polar_exchange)
+                # print("yunosu_function:",polar_exchange)
 
 
     # 結果の表示
