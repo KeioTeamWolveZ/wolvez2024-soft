596ba481a498b0a42bd9d7eec2006593e3a6c57a
hirobon1690 <hirobon1690@gmail.com>
2024-04-30 14:06:32 +0900

    outlierの計算式を変更

--- test_code/AR_Planning/AR_outlier.py
+++ test_code/AR_Planning/AR_outlier.py
@@ -1,33 +1,23 @@
 import numpy as np
 
-tvec = np.array([[1, 1, 1]])
-prev = np.array([[2.5, 1, 1]])
+tvec = np.array([1,1,1])
+prev = []
 
-def outlier(tvec, prev, standard=0.02):
-    """
-    作成：太田
-    引数：tvec:3次元座標のリスト、standard:基準値、prev:保存されているtvec
-    出力：外れ値かどうかの真偽値、new_prev:更新されたprev
-    取得した座標ベクトルが基準値を超えているかどうかを判定し、超えていた場合はFalseを返す。
-    また、Falseの場合、新しい座標ベクトルをprevに保存する。
-    """
-    # error = np.linalg.norm(tvec - prev)
-    # if error < standard:
-    #     # print("ok")
-    #     return error < standard, tvec
-    # else:
-    #     # print("error")
-    #     return error < standard, prev
+def outlier(tvec, prev, standard = 0.02):
+    """Check if the tvec is an outlier or not
+
+    Args:
+        tvec (np.array): translation vector
+        prev (list): list of previous translation vectors
+        standard (float): standard deviation
+    """ 
+
+    mean = np.array([np.mean([i[0] for i in prev]), np.mean([i[1] for i in prev]), np.mean([i[2] for i in prev])])
     
-    ## 以下、各座標に関しての判定ver
-    error = np.squeeze([t-p for t,p in zip(tvec,prev)])
-    # error = tvec - prev
-    if error[0] < standard and error[1] < standard and error[2] < standard:
-        print("ok")
-        return error[0] < standard, tvec
+    if abs(tvec[0] - mean[0]) < standard and abs(tvec[1] - mean[1]) < standard and abs(tvec[2] - mean[2]) < standard:
+        prev.append(tvec)
+        if len(prev) > 5:
+            prev.pop(0)
+        return True, tvec
     else:
-        print("error")
-        return error[0] < standard, prev
-
-
-print(outlier(tvec, prev))
+        return False
