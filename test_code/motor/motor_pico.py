b80b5d2de501194a8c7e5b2894c88cf668bbfbb7
hirobon1690 <58695125+hirobon1690@users.noreply.github.com>
2024-06-21 17:01:55 +0900

    Update motor_pico.py

--- test_code/motor/motor_pico.py
+++ test_code/motor/motor_pico.py
@@ -5,50 +5,37 @@ import time
 class motor():
     id = 0
     try:
-        uart = serial.Serial('COM5', 115200)
+        uart = serial.Serial('/dev/ttyACM1', 115200)
     except:
         print('Failed to open serial port')
+
+    def send(buf):
+        try:
+            motor.uart.write(buf.encode('utf-8'))
+        except:
+            print('Motor controller not coneected')
 
     def __init__(self, pin1=0, pin2=0, vref=0):
         self.velocity = 0
         self.id = motor.id
         print(motor.id)
         motor.id = motor.id + 1
-        buf = "%d 0 0\n" % (self.id)
-        try:
-            motor.uart.write(buf.encode('utf-8'))
-        except:
-            print('Motor controller not coneected')
+        self.send("%d 0 0\n" % (self.id))
             
     def set_id(self, id):
         self.id = id
         
     def go(self, v):
         self.velocity = v 
-        v = v * 360 / 100
-        buf = "%d %d %d\n" % (self.id, 0, int(v))
-        try:
-            motor.uart.write(buf.encode('utf-8'))
-        except:
-            print('Motor controller not coneected')
-        print(buf[:-1])
+        v = v * 360.0 / 100.0
+        self.send("%d %d %d\n" % (self.id, 0, int(v)))
 
     def go_deg_per_sec(self, v):
-        self.velocity = v * 100 / 360
-        buf = "%d %d %d\n" % (self.id, 0, int(v))
-        try:
-            motor.uart.write(buf.encode('utf-8'))
-        except:
-            print('Motor controller not coneected')
-        print(buf[:-1])
-
+        self.velocity = v * 100.0 / 360.0
+        self.go(self.velocity)
+        
     def go_deg(self, x):
-        buf = "%d %d %d\n" % (self.id, 1, x)
-        try:
-            motor.uart.write(buf.encode('utf-8'))
-        except:
-            print('Motor controller not coneected')
-        print(buf[:-1])
+        self.send("%d %d %d\n" % (self.id, 1, x))
 
     def back(self, v):
         self.go(-v)
@@ -60,20 +47,10 @@ class motor():
         if not self.velocity == 0:
             # 少しずつDuty比を落として速度を落とす、-10のところは実験によって変えられそう
             for _velocity in range(self.velocity, 0, -10):
-                buf = "%d %d %d\n" % (self.id, 0, int(_velocity*360/100))
-                try:
-                    motor.uart.write(buf.encode('utf-8'))
-                except:
-                    print('Motor controller not coneected')
-                print(buf[:-1])
+                self.go(_velocity)
                 time.sleep(0.5)
             self.velocity = 0
-        buf = "%d 0 0\n" % (self.id)
-        try:
-            motor.uart.write(buf.encode('utf-8'))
-        except:
-            print('Motor controller not coneected')
-        print("%d %d %d\n" % (self.id, 0, 0))
+        self.stop()
 
 # ブレーキ（何であるんだろう？）
     def brake(self):
