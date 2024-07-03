f561d261198708aee6f410255b8a1d5fe8701c31
suzukiYU000 <syuma2001@keio.jp>
2024-07-03 16:50:40 +0900

    pressure

--- EtoE/Wolvez2024_now/__init__.py
+++ EtoE/Wolvez2024_now/__init__.py
@@ -3,16 +3,16 @@
 # Latest Update: 2024/06/07
 print("\n__Importing Wolvez2024 package ...__\n")
 
-from .libcam_module import Picam
-from .ar_module import Target
-from .motor_power_planner import ARPowerPlanner, ColorPowerPlanner
+# from .libcam_module import Picam
+# from .ar_module import Target
+# from .motor_power_planner import ARPowerPlanner, ColorPowerPlanner
 
-from .arm import Arm
-from .gps import GPS
-from .bno055 import BNO055
-from .lora import lora
-from .motor import Motor
-from .led import led
+# from .arm import Arm
+# from .gps import GPS
+# from .bno055 import BNO055
+# from .lora import lora
+# from .motor import Motor
+# from .led import led
 
 
 __all__ = [
