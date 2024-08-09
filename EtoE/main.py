258aeaa408fa8844f2d797b32c4b29405dcfacf8
suzukiYU000 <syuma2001@keio.jp>
2024-08-09 14:23:23 +0900

    WIP

--- EtoE/main.py
+++ EtoE/main.py
@@ -5,9 +5,9 @@ import RPi.GPIO as GPIO
 
 
 
-start_state = 0
-end_state = 8
-sepa_mode  = True
+start_state = 5
+end_state = 6
+sepa_mode  = False
 try:	
 	cansat  = Cansat(start_state,sepa_mode)
 	cansat.sensor_setup()
